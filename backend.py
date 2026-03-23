4e2dfe6a9751bd13698c902bfe51a6dcd951677f
murtuza-labs <bdllhmurtuza@gmail.com>
2026-03-23 22:21:30 +0530

    Initial commit: backend and frontend working together for the first time

---
+++ backend.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template("login.html")
+
+@app.route('/home')
+def home():
+    return render_template("index.html")
+
+@app.route('/cart')
+def cart():
+    return render_template("cart.html")
+
+@app.route('/sell')
+def sell():
+    return render_template("sell.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
